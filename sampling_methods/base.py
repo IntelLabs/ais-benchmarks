- Fixed bug in MultivariateNormal distribution - Fixed problem with M-PMC. Needs K>15 to help with exploding covariance

sampling_methods/base.py
@@ -65,7 +65,7 @@ class CMixtureSamplingMethod(CSamplingMethod):
         return self.model.prob(s)
 
     def logprob(self, s):
-        return self.prob(s)
+        return np.log(self.prob(s))
 
     def draw(self, ax):
         if len(self.space_max) == 1:
@@ -76,14 +76,14 @@ class CMixtureSamplingMethod(CSamplingMethod):
 
     def draw1d(self, ax):
         res = []
-        for q in self.proposals:
+        for q, w in zip(self.model.models, self.model.weights):
             res.extend(ax.plot(q.mean.flatten(), 0, "gx", markersize=20))
-            res.extend(plot_pdf(ax, q, self.space_min, self.space_max,
-                                alpha=1.0, options="r--", resolution=0.01, scale=1/len(self.proposals)))
+            res.extend(plot_pdf(ax, q, self.space_min, self.space_max, alpha=1.0,
+                                options="r--", resolution=0.01, scale=w))
 
         # res.extend(ax.plot(q.mean.flatten(), 0, "gx", markersize=20, label="$\mu_n$"))
         res.extend(plot_pdf(ax, q, self.space_min, self.space_max, label="$q_n(x)$",
-                            alpha=1.0, options="r--", resolution=0.01, scale=1/len(self.proposals)))
+                            alpha=1.0, options="r--", resolution=0.01, scale=w))
 
         for s, w in zip(self.samples, self.weights):
             res.append(ax.vlines(s, 0, w, "g", alpha=0.1))

sampling_methods/m_pmc.py
@@ -31,7 +31,6 @@ class CMixturePMC(CMixtureSamplingMethod):
         self.model = None
         self.dims = len(space_max)
         self.reset()
-        raise NotImplementedError("This class is a tentative implementation. Is not finalized or tested")
 
     def reset(self):
         super(self.__class__, self).reset()
@@ -56,14 +55,8 @@ class CMixturePMC(CMixtureSamplingMethod):
             self.wproposals[d] = np.sum(weights * posteriors[d])
 
             # Update proposal parameters (gaussian proposal)
-            # Mean eq. 14.2. TODO: Vectorize this calculation
-            mu = np.zeros(self.dims)
-            for i in range(len(weights)):
-                w = weights[i]
-                s = samples[i]
-                rho = posteriors[d][i]
-                mu += w * rho * s
-            mu /= self.wproposals[d]
+            # Mean eq. 14.2
+            mu = np.sum(weights.reshape(-1, 1) * posteriors[d].reshape(-1, 1) * samples, axis=0) / self.wproposals[d]
 
             # Covariance eq. 14.3
             cov_val = (samples - mu).T @ (samples - mu)
@@ -72,13 +65,15 @@ class CMixturePMC(CMixtureSamplingMethod):
                 cov += weights[i] * posteriors[d][i] * cov_val
             cov /= self.wproposals[d]
 
-            if np.any(np.isnan(cov)):
+            # Failsafe for collapsing or exploding covariances
+            if np.any(np.isnan(cov)) or np.any(cov > 1):
                 cov = np.diag(np.ones(self.dims) * 0.01)
 
             self.proposals[d].set_moments(mu, cov)
 
         # Renormalize proposals weights
         self.wproposals = self.wproposals / self.wproposals.sum()
+        self.model.weights = self.wproposals
 
     def importance_sample(self, target_d, n_samples, timeout=60):
         elapsed_time = 0