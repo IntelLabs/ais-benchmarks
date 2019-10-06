Fixed a bug of the volume computation in MultivariateUniform. This combined with minor modifications solved the 2D visualization of the TP sampling.

distributions/CMultivariateUniform.py
@@ -7,7 +7,7 @@ class CMultivariateUniform:
 
         self.center = center
         self.radius = radius
-        self.volume = np.prod(radius*2)
+        self.volume = np.prod(np.array([radius*2]*len(center)))
 
     def sample(self, n_samples=1):
         minval = self.center - self.radius
@@ -21,12 +21,11 @@ class CMultivariateUniform:
     def logprob(self, samples):
         return np.log(self.prob(samples))
 
-    # TODO: MAKE THIS WORK FOR THE MULTIDIMENSIONAL CASE.
     def prob(self, samples):
         if len(samples.shape) == 1:
-            samples = samples.reshape(1, self.dims, 1)
+            samples = samples.reshape(1, self.dims)
         elif len(samples.shape) == 2:
-            samples = samples.reshape(len(samples), self.dims, 1)
+            samples = samples.reshape(len(samples), self.dims)
         else:
             raise ValueError("Shape of samples does not match self.dims")
 

sampling_methods/tree_pyramid.py
@@ -44,7 +44,7 @@ class CTreePyramidNode:
         if kernel == "haar":
             self.sampler = CMultivariateUniform(self.center, self.radius)
         elif kernel == "normal":
-            self.sampler = CMultivariateNormal(self.center, np.diag(t_tensor([self.radius/self.bw_div])))
+            self.sampler = CMultivariateNormal(self.center, np.diag(t_tensor([self.radius/self.bw_div] * len(self.center))))
         else:
             raise ValueError("Unknown kernel type. Must be 'normal' or 'haar'")
 
@@ -64,8 +64,8 @@ class CTreePyramidNode:
         :param importance_d: Importance distribution used to generate the sample. In the paper: q(x)
         :param target_f: Target f(x) used for importance sampling.
         """
-        # self.weight = (target_f(self.coords) * target_d.prob(self.coords)) / importance_d.prob(self.coords)
-        self.weight = (target_f(self.center) * target_d.prob(self.center)) / importance_d.prob(self.center)
+        self.weight = (target_f(self.coords) * target_d.prob(self.coords)) / importance_d.prob(self.coords)
+        # self.weight = (target_f(self.center) * target_d.prob(self.center)) / importance_d.prob(self.center)
         self.value = self.weight * (self.radius ** len(self.center))
         self.weight_hist.append(self.weight)
 
@@ -210,7 +210,7 @@ class CTreePyramidSampling(CSamplingMethod):
         return samples, weights
 
     def prob(self, s):
-        prob = 0
+        prob = np.zeros(len(s))
         for i, n in enumerate(self.T.leaves):
             prob = prob + n.sampler.prob(s) * n.weight
             self._num_q_evals += 1

utils/plot_utils.py
@@ -26,9 +26,9 @@ def make_grid(space_min, space_max, resolution):
 
 
 def grid_sample_distribution(dist, space_min, space_max, resolution):
-    grid, dims, shape= make_grid(space_min, space_max, resolution)
-    log_prob = dist.logprob(t_tensor(grid))
-    return grid, log_prob, dims, shape
+    grid, dims, shape = make_grid(space_min, space_max, resolution)
+    prob = dist.prob(t_tensor(grid))
+    return grid, prob, dims, shape
 
 
 def plot_pdf(ax, pdf, space_min, space_max, resolution=0.1, options="-b", alpha=0.2, scale=1.0, label=None):
@@ -66,8 +66,8 @@ def plot_grid_sampled_pdfs(ax, dims, prob_p, shape=None, marginalize_axes=None, 
 
 
 def plot_pdf2d(ax, pdf, space_min, space_max, resolution=0.1, alpha=0.2, scale=1.0, label=None, colormap=plt.cm.hot):
-    grid, log_prob, dims, shape = grid_sample_distribution(pdf, space_min, space_max, resolution=resolution)
-    return plot_grid_sampled_pdfs(ax, dims, np.exp(log_prob) * scale, shape=shape, alpha=alpha, cmap=colormap, label=label)
+    grid, prob, dims, shape = grid_sample_distribution(pdf, space_min, space_max, resolution=resolution)
+    return plot_grid_sampled_pdfs(ax, dims, prob * scale, shape=shape, alpha=alpha, cmap=colormap, label=label)
 
 
 def plot_ellipsoids(axis, ellipsoids, points):
@@ -124,8 +124,8 @@ def plot_tpyramid_volume(axis, T):
         min_coord = n.center - n.radius
         max_coord = n.center + n.radius
         z0 = 0
-        z1 = n.value/((2*n.radius)**len(n.center))
-        rect_prism(axis, [min_coord[0],max_coord[0]], [min_coord[1],max_coord[1]], [z0,z1])
+        height = n.weight * n.sampler.prob(n.center)
+        rect_prism(axis, [min_coord[0],max_coord[0]], [min_coord[1],max_coord[1]], [z0,height])
 
 
 def plot_grid_area(axis, samples, values, resolution):