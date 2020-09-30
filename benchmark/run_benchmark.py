5aa33e63ea1f0fb93096da854b65608dc4a3ea54
jfelip <javier.felip.leon@gmail.com>
2020-09-30 15:43:26 -0700

    Show evaluation time in h:m:s

--- benchmark/run_benchmark.py
+++ benchmark/run_benchmark.py
@@ -6,7 +6,7 @@ from benchmark.CBenchmark import CBenchmark
 benchmark = CBenchmark()
 
 # TODO: make this paths relative to this file
-b_file = "def_benchmark_benchden.yaml"
+b_file = "def_benchmark.yaml"
 m_file = "def_methods.yaml"
 
 if len(sys.argv) == 3:
@@ -17,4 +17,9 @@ time_eval = time.time()
 
 benchmark.run(benchmark_file=b_file, methods_file=m_file)
 
-print("TOTAL EVALUATION TIME: %f hours" % ((time.time() - time_eval) / 3600.0))
+total_time = time.time() - time_eval
+hours = total_time // 3600
+mins = (total_time - hours * 3600) // 60
+secs = total_time - hours * 3600 - mins * 60
+
+print("TOTAL EVALUATION TIME: %dh %dm %5.3fs" % (hours, mins, secs))
