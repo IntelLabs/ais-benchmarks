67580e17a9694990f8320cea5e88c698765bdf6a
jfelip <javier.felip.leon@gmail.com>
2020-10-15 17:24:47 -0700

    Separate experiment config file from target dist definitions

--- benchmark/run_benchmark.py
+++ benchmark/run_benchmark.py
@@ -1,25 +1,26 @@
 import time
 import sys
 from benchmark.CBenchmark import CBenchmark
+from utils.misc import time_to_hms
 
 
 benchmark = CBenchmark()
 
 # TODO: make this paths relative to this file
-b_file = "def_benchmark.yaml"
-m_file = "def_methods.yaml"
+b_file = "tp_ais_experiments/def_benchmark_gmm.yaml"
+m_file = "tp_ais_experiments/def_methods.yaml"
+c_file = "tp_ais_experiments/def_config_test.yaml"
 
-if len(sys.argv) == 3:
+if len(sys.argv) == 4:
     b_file = sys.argv[1]
     m_file = sys.argv[2]
+    c_file = sys.argv[3]
 
 time_eval = time.time()
 
-benchmark.run(benchmark_file=b_file, methods_file=m_file)
+benchmark.run(benchmark_file=b_file, methods_file=m_file, config_file=c_file)
 
 total_time = time.time() - time_eval
-hours = total_time // 3600
-mins = (total_time - hours * 3600) // 60
-secs = total_time - hours * 3600 - mins * 60
+h, m, s = time_to_hms(total_time)
 
-print("TOTAL EVALUATION TIME: %dh %dm %5.3fs" % (hours, mins, secs))
+print("TOTAL EVALUATION TIME: %dh %dm %5.3fs" % (h, m, s))
