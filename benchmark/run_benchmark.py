1267a32c4abc0bd268ed2dd57d52c99c688b6447
jfelip <javier.felip.leon@gmail.com>
2020-10-16 17:07:54 -0700

    Separated experiments in multiple config files to allow massive parallel execution.
    Evaluation of HiDaisee with different ESS targets, TP-AIS with different ESS targets and without ESS target and evaluate all the variation on 1,2,3,4,5,6 dimensional gmm, normal and egg distributions

--- benchmark/run_benchmark.py
+++ benchmark/run_benchmark.py
@@ -10,15 +10,17 @@ benchmark = CBenchmark()
 b_file = "tp_ais_experiments/def_benchmark_gmm.yaml"
 m_file = "tp_ais_experiments/def_methods.yaml"
 c_file = "tp_ais_experiments/def_config_test.yaml"
+out_file = "tp_ais_experiments/results.txt"
 
-if len(sys.argv) == 4:
+if len(sys.argv) == 5:
     b_file = sys.argv[1]
     m_file = sys.argv[2]
     c_file = sys.argv[3]
+    out_file = sys.argv[4]
 
 time_eval = time.time()
 
-benchmark.run(benchmark_file=b_file, methods_file=m_file, config_file=c_file)
+benchmark.run(benchmark_file=b_file, methods_file=m_file, config_file=c_file, out_file=out_file)
 
 total_time = time.time() - time_eval
 h, m, s = time_to_hms(total_time)
---
+++ benchmark/tp_ais_experiments/hidaisee/run_experiment_normal.sh
@@ -0,0 +1,73 @@
+###
+## THIS SCRIPT SPAWNS 16 EVALUATION PROCESSES
+###
+
+# Evaluate all HiDaisee with 1,2,3 Dimension normal benchmark
+python3 run_benchmark.py tp_ais_experiments/benchmarks/def_benchmark_normal_123D.yaml \
+tp_ais_experiments/hidaisee/def_methods_all.yaml tp_ais_experiments/hidaisee/def_config.yaml \
+tp_ais_experiments/hidaisee/results_normal_123D.txt  2>tp_ais_experiments/hidaisee/results_normal_123D_err.txt &
+
+# Evaluate HiDaisee with ESS 60% and 4,5,6 Dimension normal benchmark
+python3 run_benchmark.py tp_ais_experiments/benchmarks/def_benchmark_normal_4D.yaml \
+tp_ais_experiments/hidaisee/def_methods_HD60.yaml tp_ais_experiments/hidaisee/def_config.yaml \
+tp_ais_experiments/hidaisee/results_normal_HD60_4D.txt  2>tp_ais_experiments/hidaisee/results_normal_HD60_4D_err.txt &
+
+python3 run_benchmark.py tp_ais_experiments/benchmarks/def_benchmark_normal_5D.yaml \
+tp_ais_experiments/hidaisee/def_methods_HD60.yaml tp_ais_experiments/hidaisee/def_config.yaml \
+tp_ais_experiments/hidaisee/results_normal_HD60_5D.txt  2>tp_ais_experiments/hidaisee/results_normal_HD60_5D_err.txt &
+
+python3 run_benchmark.py tp_ais_experiments/benchmarks/def_benchmark_normal_6D.yaml \
+tp_ais_experiments/hidaisee/def_methods_HD60.yaml tp_ais_experiments/hidaisee/def_config.yaml \
+tp_ais_experiments/hidaisee/results_normal_HD60_6D.txt  2>tp_ais_experiments/hidaisee/results_normal_HD60_6D_err.txt &
+
+# Evaluate HiDaisee with ESS 70% and 4,5,6 Dimension normal benchmark
+python3 run_benchmark.py tp_ais_experiments/benchmarks/def_benchmark_normal_4D.yaml \
+tp_ais_experiments/hidaisee/def_methods_HD70.yaml tp_ais_experiments/hidaisee/def_config.yaml \
+tp_ais_experiments/hidaisee/results_normal_HD70_4D.txt  2>tp_ais_experiments/hidaisee/results_normal_HD70_4D_err.txt &
+
+python3 run_benchmark.py tp_ais_experiments/benchmarks/def_benchmark_normal_5D.yaml \
+tp_ais_experiments/hidaisee/def_methods_HD70.yaml tp_ais_experiments/hidaisee/def_config.yaml \
+tp_ais_experiments/hidaisee/results_normal_HD70_5D.txt  2>tp_ais_experiments/hidaisee/results_normal_HD70_5D_err.txt &
+
+python3 run_benchmark.py tp_ais_experiments/benchmarks/def_benchmark_normal_6D.yaml \
+tp_ais_experiments/hidaisee/def_methods_HD70.yaml tp_ais_experiments/hidaisee/def_config.yaml \
+tp_ais_experiments/hidaisee/results_normal_HD70_6D.txt  2>tp_ais_experiments/hidaisee/results_normal_HD70_6D_err.txt &
+
+# Evaluate HiDaisee with ESS 80% and 4,5,6 Dimension normal benchmark
+python3 run_benchmark.py tp_ais_experiments/benchmarks/def_benchmark_normal_4D.yaml \
+tp_ais_experiments/hidaisee/def_methods_HD80.yaml tp_ais_experiments/hidaisee/def_config.yaml \
+tp_ais_experiments/hidaisee/results_normal_HD80_4D.txt  2>tp_ais_experiments/hidaisee/results_normal_HD80_4D_err.txt &
+
+python3 run_benchmark.py tp_ais_experiments/benchmarks/def_benchmark_normal_5D.yaml \
+tp_ais_experiments/hidaisee/def_methods_HD80.yaml tp_ais_experiments/hidaisee/def_config.yaml \
+tp_ais_experiments/hidaisee/results_normal_HD80_5D.txt  2>tp_ais_experiments/hidaisee/results_normal_HD80_5D_err.txt &
+
+python3 run_benchmark.py tp_ais_experiments/benchmarks/def_benchmark_normal_6D.yaml \
+tp_ais_experiments/hidaisee/def_methods_HD80.yaml tp_ais_experiments/hidaisee/def_config.yaml \
+tp_ais_experiments/hidaisee/results_normal_HD80_6D.txt  2>tp_ais_experiments/hidaisee/results_normal_HD80_6D_err.txt &
+
+# Evaluate HiDaisee with ESS 90% and 4,5,6 Dimension normal benchmark
+python3 run_benchmark.py tp_ais_experiments/benchmarks/def_benchmark_normal_4D.yaml \
+tp_ais_experiments/hidaisee/def_methods_HD90.yaml tp_ais_experiments/hidaisee/def_config.yaml \
+tp_ais_experiments/hidaisee/results_normal_HD90_4D.txt  2>tp_ais_experiments/hidaisee/results_normal_HD90_4D_err.txt &
+
+python3 run_benchmark.py tp_ais_experiments/benchmarks/def_benchmark_normal_5D.yaml \
+tp_ais_experiments/hidaisee/def_methods_HD90.yaml tp_ais_experiments/hidaisee/def_config.yaml \
+tp_ais_experiments/hidaisee/results_normal_HD90_5D.txt  2>tp_ais_experiments/hidaisee/results_normal_HD90_5D_err.txt &
+
+python3 run_benchmark.py tp_ais_experiments/benchmarks/def_benchmark_normal_6D.yaml \
+tp_ais_experiments/hidaisee/def_methods_HD90.yaml tp_ais_experiments/hidaisee/def_config.yaml \
+tp_ais_experiments/hidaisee/results_normal_HD90_6D.txt  2>tp_ais_experiments/hidaisee/results_normal_HD90_6D_err.txt &
+
+# Evaluate HiDaisee with ESS 95% and 4,5,6 Dimension normal benchmark
+python3 run_benchmark.py tp_ais_experiments/benchmarks/def_benchmark_normal_4D.yaml \
+tp_ais_experiments/hidaisee/def_methods_HD95.yaml tp_ais_experiments/hidaisee/def_config.yaml \
+tp_ais_experiments/hidaisee/results_normal_HD95_4D.txt  2>tp_ais_experiments/hidaisee/results_normal_HD95_4D_err.txt &
+
+python3 run_benchmark.py tp_ais_experiments/benchmarks/def_benchmark_normal_5D.yaml \
+tp_ais_experiments/hidaisee/def_methods_HD95.yaml tp_ais_experiments/hidaisee/def_config.yaml \
+tp_ais_experiments/hidaisee/results_normal_HD95_5D.txt  2>tp_ais_experiments/hidaisee/results_normal_HD95_5D_err.txt &
+
+python3 run_benchmark.py tp_ais_experiments/benchmarks/def_benchmark_normal_6D.yaml \
+tp_ais_experiments/hidaisee/def_methods_HD95.yaml tp_ais_experiments/hidaisee/def_config.yaml \
+tp_ais_experiments/hidaisee/results_normal_HD95_6D.txt  2>tp_ais_experiments/hidaisee/results_normal_HD95_6D_err.txt &
