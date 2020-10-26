67cba8fa6a8ab470878cf98b2fb3f08de030b74f
jfelip <javier.felip.leon@gmail.com>
2020-10-26 13:12:21 -0700

    Removed the space size parameter from distributions. Now the support is automatically used.
    Documented and clean up the evaluation function. Needs extension to enable back the visualizations.

--- benchmark/evaluation.py
+++ benchmark/evaluation.py
@@ -1,7 +1,5 @@
 import numpy as np
 import time
-import gc
-from matplotlib import pyplot as plt
 import cProfile
 import pstats
 import tracemalloc
@@ -10,18 +8,13 @@ from scipy.stats import entropy
 
 from sampling_methods.base import t_tensor
 from sampling_methods.base import uniform_sample_distribution
-from sampling_methods.base import grid_sample_distribution
 from distributions.nonparametric.CKernelDensity import CKernelDensity
-from utils.plot_utils import plot_grid_sampled_pdfs
-from utils.plot_utils import plot_pdf
-from utils.video_writer import CVideoWriter
 from utils.misc import time_to_hms
 
 
 def log_print(text, file, mode='a+'):
     with open(file, mode=mode) as f:
         f.write(text + "\n")
-        #print(text)
 
 
 def bhattacharyya_distance(p_samples_prob, q_samples_prob):
@@ -113,158 +106,148 @@ def evaluate_samples(samples, samples_logprob, target_dist, space_min, space_max
     return evaluate_proposal(approximate_pdf, target_dist, space_min, space_max, sampling_eval_samples)
 
 
-def evaluate_method(ndims, space_size, target_dist, sampling_method, max_samples, sampling_eval_samples,
-                    metrics=["NESS", "JSD", "T"], rseed=0, n_reps=10, batch_size=1,
-                    debug=True, filename=None, max_sampling_time=600, videofile=None):
+def evaluate_method(ndims, support, target_dist, sampling_method, max_samples, sampling_eval_samples,
+                    metrics=["NESS", "JSD", "T"], rseed=0, n_reps=10, batch_size=16,
+                    debug=True, filename=None, max_sampling_time=600, profile=False):
+    """
+    Parameters
+    ----------
+    ndims : int
+        Number of dimensions of the target distribution
 
-    # TODO: This is a central function in the framework needs serious cleaning and documentation
+    support : array_like
+        Support limits. support[0] contains the lower limits and support[1] the upper limits for the support of the
+        target distribution. The support limits are used to generate the samples for the approximate JSD and other
+        MC approximations for the metrics. If the target has infinite support, a reasonable support containing most
+        of the probability mass needs to be specified.
 
-    np.random.seed(rseed)
-    batch_samples = batch_size
-    space_min = t_tensor([-space_size] * ndims)
-    space_max = t_tensor([space_size] * ndims)
+    target_dist : CDistribution
+        Target distribution under evaluation. This can be an object derived from CDistribution or any other object that
+        implements prob(x) and log_prob(x) methods where x is a batch of samples.
 
-    samples_acc = np.random.uniform(space_min, space_max).reshape(-1, ndims)
-    samples_logprob_acc = target_dist.log_prob(samples_acc)
+    sampling_method : CSamplingMethod
+        Object derived from CSamplingMethod that represents/implements the method to be evaluated.
 
-    # Frame collection for the videofile
-    if videofile is not None:
-        vid_writer = CVideoWriter(videofile, fps=3)
+    max_samples : int
+        Number of samples to obtain with the sampling algorithm to conclude the evaluation.
 
-    if debug:
-        pts = []
-        if ndims == 1:
-            fig = plt.figure(figsize=(10, 8))
-            ax = plt.subplot(111)
-            # plt.hold(True)
-            plt.show(block=False)
-            plot_pdf(ax, target_dist, space_min, space_max, alpha=1.0, options="b-", resolution=0.01, label="$\pi(x)$")
+    sampling_eval_samples : int
+        Number of Monte Carlo samples used to approximate the measured metrics.
 
-            plt.xlim(space_min, space_max)
-            plt.ylim(0, ax.get_ylim()[1])
+    metrics : list[str]
+        List of strings that specify the metrics to be computed. Default: ["NESS", "JSD", "T"]
 
-        elif ndims == 2:
-            fig = plt.figure(figsize=(10, 8))
-            ax = plt.subplot(111)
-            # ax = plt.subplot(111, projection='3d')
-            # plt.hold(True)
-            plt.show(block=False)
+    rseed: int
+        Random seed to be used. If none is specified, the random generator state is not altered. Default: None
 
-            grid, log_prob, dims, shape = grid_sample_distribution(target_dist, space_min, space_max, resolution=0.02)
-            plot_grid_sampled_pdfs(ax, dims, np.exp(log_prob), shape=shape, alpha=1, label="$\pi(x)$", cmap='gray', linestyles='dashed')
+    n_reps: int
+        Number of times to repeat each experiment. Default: 10.
 
-            plt.xlim(space_min[0], space_max[0])
-            plt.ylim(space_min[1], space_max[1])
-            # ax.set_zlim(ax.get_zlim())
+    batch_size: int
+        Number of samples to be generated before computing the partial metrics. Default: 16.
+
+    debug: bool
+        Flag to enable console debug messages and other debugging visualizations. Default: False.
+
+    filename : str
+        Path to store the experiment results, use None to disable result saving. Default: None.
+
+    max_sampling_time : float
+        Time limit in seconds for the generation of max_samples using the specified sampling_method. When the timeout
+        is reached the sampling process will be aborted and the partial results written to disk. Default: 600.0
+
+    profile : bool
+        Flag to enable profiling results. Consider using when debugging or analyzing an algorithm as the usage of the
+        profiler might impact the performance. Default: False.
+
+    Returns
+    -------
+
+    Raises
+    -------
+    None
+    """
+
+    # Set the random seed if specified, important for reproducibility
+    if rseed is not None:
+        np.random.seed(rseed)
 
     # Start profiling tools
-    profiler = cProfile.Profile()
-    profiler.enable()
+    if profile:
+        profiler = cProfile.Profile()
+        profiler.enable()
+
+    # Start memory profiling
     tracemalloc.start()
 
+    # Repeat the experiment n_reps times
     for nexp in range(n_reps):
-        # print("Experiment %d/%d || method: %s || dist: %s || dims: %d " % (nexp+1, n_reps, sampling_method.name, target_dist.name, ndims), end="", flush=True)
-        t_start = time.time()
-        mem_peak = 0
 
-        # Perform sampling
+        # Initialize sampling variables
+        t_start = time.time()
         sampling_time = 0
         sampling_method.reset()
         samples_acc = t_tensor([])
-        samples_weights_acc = t_tensor([])
-        # pts = []
-        n_samples = batch_samples
+        n_samples = batch_size
+
+        # Perform importance sampling until the desired number of samples is obtained
         while len(samples_acc) < max_samples:
 
+            # Compute current memory usage in MB
             mem_used = tracemalloc.get_tracemalloc_memory()/(1024.0*1024.0)
-            mem_peak = mem_used if mem_used > mem_peak else mem_peak
 
+            # Obtain experiment execution runtime
             h, m, s = time_to_hms(time.time() - t_start)
+
+            # Display partial sampling experiment statistics
             print("Exp %02d/%02d || %s || %s || %dD || #s: %.1fk || %5.1f%% || t: %02dh %02dm %4.1fs || mem: %.1fMB" % (
                 nexp + 1, n_reps, sampling_method.name, target_dist.name, ndims, len(samples_acc)/1000.0,
                 (len(samples_acc)/max_samples)*100, h, m, s, mem_used), end="\r", flush=True)
 
+            # Perform importance sampling operation to generate a batch of samples and compute the time taken
             t_ini = time.time()
-
-            del samples_acc
-            del samples_weights_acc
-            samples_acc, samples_weights_acc = sampling_method.importance_sample(target_d=target_dist,
-                                                                                 n_samples=n_samples,
-                                                                                 timeout=max_sampling_time - sampling_time)
-
-            # samples_logprob_acc = sampling_method.log_prob(samples_acc)
-
-            n_samples = len(samples_acc) + batch_samples
-
+            samples_acc, _ = sampling_method.importance_sample(target_d=target_dist, n_samples=n_samples,
+                                                               timeout=max_sampling_time - sampling_time)
             sampling_time += time.time()-t_ini
 
-            # sampling_method._update_model()
+            # Sampling methods generate a desired number of samples and maintain the state. Therefore it is not possible
+            # to ask the sampling algos for batch_size samples every time, thus in order to have partial results during
+            # the sampling experiments, the number of samples to generate is always increase by batch size. This lets
+            # us measure partial results in increments of batch_size samples.
+            n_samples = len(samples_acc) + batch_size
 
+            # Perform sampling evaluation and store results to file
             if filename is not None:
-                # t_ini = time.time()
+                # TODO: Compute only desired metrics
                 [js_div, bhattacharyya_dist, ev_mse] = \
-                    evaluate_proposal(sampling_method, target_dist, space_min, space_max, sampling_eval_samples)
-                # print("Evaluation time: %5.3f nsamples: %d samples/sec: %5.3f" % (time.time() - t_ini, len(samples_acc), len(samples_acc) / (time.time() - t_ini)))
+                    evaluate_proposal(sampling_method, target_dist, support[0], support[1], sampling_eval_samples)
 
                 log_print("%02d %04d %7.5f %7.5f %8.5f %7.5f %7.4f %s %s %5.3f %d %d %d %f7.2" % (
                     ndims, len(samples_acc), js_div, bhattacharyya_dist, ev_mse, sampling_method.get_NESS(), sampling_time,
                     sampling_method.name, target_dist.name, sampling_method.get_acceptance_rate(),
                     sampling_method.num_proposal_samples, sampling_method.num_proposal_evals, sampling_method.num_target_evals, mem_used), file=filename)
 
-                if debug:
-                    plt.suptitle("%s | #smpl: %d | JSD: %3.3f | BHT: %3.3f | NESS: %.3f | AcceptRatio:%3.3f" %
-                                 (sampling_method.name, n_samples, js_div, bhattacharyya_dist, sampling_method.get_NESS(),
-                                  sampling_method.get_acceptance_rate()))
-
+            # Some sampling algorithms may take too long to generate the desired number of samples, the timeout ensures
+            # the operation will end if the specified timeout is reached.
             if sampling_time > max_sampling_time:
                 break
 
-            # DEBUG CODE HERE
-            if debug:
-                # Remove previous points
-                for element in pts:
-                    element.remove()
-                pts.clear()
-                if ndims == 1:
-                    pts.extend(sampling_method.draw(ax))
-                    # pts.extend(ax.plot(samples_acc, samples_logprob_acc, "r."))
-                    pts.extend(ax.plot(samples_acc, np.ones(len(samples_acc))*0.1, "r|", label="samples"))
-                    plt.pause(0.01)
-                    if videofile is not None:
-                        vid_writer.add_frame(fig)
-                if ndims == 2:
-                    pts.extend(sampling_method.draw(ax))
-                    pts.append(ax.scatter(samples_acc[:, 0], samples_acc[:, 1], label="samples", c="r", marker=".", alpha=0.4))
-                    if videofile is not None:
-                        vid_writer.add_frame(fig)
-                    plt.pause(0.01)
-                plt.legend(framealpha=0.5, loc="best")
-
-                # Force garbage collection
-                # gc.collect()
-
+        # Obtain experiment execution runtime
         h, m, s = time_to_hms(time.time() - t_start)
+
+        # Print final experiment statistics
         print(
             "Exp %02d/%02d || %s || %s || %dD || #s: %.1fk || %5.1f%% || t: %02dh %02dm %4.1fs || mem: %.1fMB" % (
                 nexp + 1, n_reps, sampling_method.name, target_dist.name, ndims, len(samples_acc) / 1000.0,
                 (len(samples_acc) / max_samples) * 100, h, m, s, mem_used), end="\n", flush=True)
-        # print(
-        #     "Exp %02d/%02d || %s || dist: %s || dims: %d || #samples: %.1fk || %5.1f%% || t: %02dh %02dm %4.1fs || mem" % (
-        #         nexp + 1, n_reps, sampling_method.name, target_dist.name, ndims, len(samples_acc) / 1000.0,
-        #         (len(samples_acc) / max_samples) * 100, h, m, s), end="\n", flush=True)
 
-        if debug and (ndims == 1 or ndims == 2):
-            if videofile is not None:
-                vid_writer.add_frame(fig)
-                vid_writer.save()
-
+    # Stop profiling tools and save results
     tracemalloc.stop()
-    profiler.disable()
-    ps = pstats.Stats(profiler)
-    ps.sort_stats("cumtime")
-    ps.print_stats()
+    if profile:
+        profiler.disable()
+        ps = pstats.Stats(profiler)
+        ps.sort_stats("cumtime")
+        ps.print_stats()
 
-    if debug and (ndims == 1 or ndims == 2):
-        plt.close()
-
+    # Return sampling visualization data
     return sampling_method.get_viz_frames()
