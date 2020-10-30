d4d5425905009b0286890586d489cf41e861affc
jfelip <javier.felip.leon@gmail.com>
2020-10-30 15:46:28 -0700

    Add pre, post and reset methods to enable the implementation of more metrics that take measurements before sampling and after sampling to evaluate the changes. For example, runtime, memory.

--- metrics/base.py
+++ metrics/base.py
@@ -3,7 +3,16 @@ class CMetric:
     def __init__(self):
         self.name = "base"
         self.type = "base"
+        self.value = 0
 
-    def compute(self, params):
+    def compute(self, **kwargs):
         raise NotImplementedError
 
+    def pre(self, **kwargs):
+        raise NotImplementedError
+
+    def post(self, **kwargs):
+        raise NotImplementedError
+
+    def reset(self, **kwargs):
+        raise NotImplementedError
