c7017297dde2ef81447a95f81be0f09d6a8f20cc
jfelip <javier.felip.leon@gmail.com>
2020-10-30 15:45:04 -0700

    Initial implementation of memory usage metric

--- metrics/performance.py
+++ metrics/performance.py
@@ -1,3 +1,52 @@
 """
 Performance metrics will evaluate computation complexity, memory consumption and efficiency (power consumption)
 """
+
+from metrics.base import CMetric
+import tracemalloc
+
+
+class CMemoryUsage(CMetric):
+    def __init__(self):
+        super().__init__()
+        self.name = "memory"
+        self.type = "performance"
+        self.mem_ini_blk = 0
+        self.mem_ini_peak_blk = 0
+        self.mem_ini = 0
+        self.mem_end_blk = 0
+        self.mem_end_peak_blk = 0
+        self.mem_end = 0
+        self.mem_cum = 0
+        self.mem_cum_blk = 0
+        self.mem_cum_peak_blk = 0
+        tracemalloc.start()
+
+    def __del__(self):
+        tracemalloc.stop()
+
+    def pre(self, **kwargs):
+        self.mem_ini_blk, self.mem_ini_peak_blk = tracemalloc.get_traced_memory()
+        self.mem_ini = tracemalloc.get_tracemalloc_memory()
+
+    def post(self, **kwargs):
+        self.mem_end_blk, self.mem_end_peak_blk = tracemalloc.get_traced_memory()
+        self.mem_end = tracemalloc.get_tracemalloc_memory()
+        self.mem_cum += self.mem_end - self.mem_ini
+        self.mem_cum_blk += self.mem_end_blk - self.mem_ini_blk
+        self.value = self.mem_cum / (1024.0*1024.0)
+
+    def compute(self, **kwargs):
+        return self.value
+
+    def reset(self, **kwargs):
+        self.mem_ini_blk = 0
+        self.mem_ini_peak_blk = 0
+        self.mem_ini = 0
+        self.mem_end_blk = 0
+        self.mem_end_peak_blk = 0
+        self.mem_end = 0
+        self.mem_cum = 0
+        self.mem_cum_blk = 0
+        self.mem_cum_peak_blk = 0
+
