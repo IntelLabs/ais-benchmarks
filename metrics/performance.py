adc89cd98731405bef4d0e44a0bb7132deb2a263
Javier Felip Leon <javier.felip.leon@gmail.com>
2020-11-12 22:45:00 -0800

    Fixed Memory measurement after realizing that the measurements were not correct with tracemalloc. Requires installation of guppy3

--- metrics/performance.py
+++ metrics/performance.py
@@ -3,9 +3,9 @@ Performance metrics will evaluate computation complexity, memory consumption and
 """
 
 import time
-import tracemalloc
 import gc
 import numpy as np
+from guppy import hpy
 
 from metrics.base import CMetric
 
@@ -54,45 +54,27 @@ class CMemoryUsage(CMetric):
         super().__init__()
         self.name = "memory"
         self.type = "performance"
-        self.mem_ini_blk = 0
-        self.mem_ini_peak_blk = 0
         self.mem_ini = 0
-        self.mem_end_blk = 0
-        self.mem_end_peak_blk = 0
         self.mem_end = 0
         self.mem_cum = 0
-        self.mem_cum_blk = 0
-        self.mem_cum_peak_blk = 0
-        tracemalloc.start()
-
-    def __del__(self):
-        tracemalloc.stop()
+        self.hpy = hpy()
 
     def pre(self, **kwargs):
         gc.collect()
-        self.mem_ini_blk, self.mem_ini_peak_blk = tracemalloc.get_traced_memory()
-        self.mem_ini = tracemalloc.get_tracemalloc_memory()
+        self.mem_ini = self.hpy.heap().size
 
     def post(self, **kwargs):
         gc.collect()
-        self.mem_end_blk, self.mem_end_peak_blk = tracemalloc.get_traced_memory()
-        self.mem_end = tracemalloc.get_tracemalloc_memory()
+        self.mem_end = self.hpy.heap().size
         self.mem_cum += self.mem_end - self.mem_ini
-        self.mem_cum_blk += self.mem_end_blk - self.mem_ini_blk
-        self.value = self.mem_cum / (1024.0*1024.0)
+        self.value = self.mem_cum / 10**6
 
     def compute(self, **kwargs):
         return self.value
 
     def reset(self, **kwargs):
-        self.mem_ini_blk = 0
-        self.mem_ini_peak_blk = 0
         self.mem_ini = 0
-        self.mem_end_blk = 0
-        self.mem_end_peak_blk = 0
         self.mem_end = 0
         self.mem_cum = 0
-        self.mem_cum_blk = 0
-        self.mem_cum_peak_blk = 0
         gc.collect()
 
