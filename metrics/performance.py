a69a82d0718e7126094e12b03ec08ca5d5b4e44c
Javier Felip Leon <javier.felip.leon@gmail.com>
2020-11-12 17:23:14 -0800

    Compute time measurement overhead and subtract it when measuring

--- metrics/performance.py
+++ metrics/performance.py
@@ -5,6 +5,7 @@ Performance metrics will evaluate computation complexity, memory consumption and
 import time
 import tracemalloc
 import gc
+import numpy as np
 
 from metrics.base import CMetric
 
@@ -16,13 +17,28 @@ class CElapsedTime(CMetric):
         self.type = "performance"
         self.t_ini = 0
         self.t_end = 0
+        self.overhead = 0
+        print("CElapsedTime initialize metric overhead estimate...")
+
+        # Compute measurement overhead to subtract it when measuring.
+        n_samples = 200
+        delta_t = 0.01
+        vals = np.zeros(n_samples)
+        for i in range(n_samples):
+            self.pre()
+            time.sleep(delta_t)
+            self.post()
+            vals[i] = self.compute()
+            self.reset()
+        self.overhead = (vals.mean() - delta_t) / n_samples
+        print("CElapsedTime overhead estimate: ", self.overhead)
 
     def pre(self, **kwargs):
         self.t_ini = time.time()
 
     def post(self, **kwargs):
         self.t_end = time.time()
-        self.value += self.t_end - self.t_ini
+        self.value += self.t_end - self.t_ini - self.overhead
 
     def compute(self, **kwargs):
         return self.value
