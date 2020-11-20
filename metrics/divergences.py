Fix KL tests and computation

metrics/divergences.py
@@ -99,11 +99,57 @@ class CKLDivergence(CDivergence):
         self.disjoint_support = False
 
     def compute_from_samples(self, p, q, samples):
-        # return self.compute_from_logsamples(p, q, samples)
-        return self.compute_from_exp_samples(p, q, samples)
+        # Obtain sample probabilities
+        # p_samples_prob = p.prob(samples)
+        # q_samples_prob = q.prob(samples)
+        # self.value = np.sum(self.compute_from_probs(p_samples_prob, q_samples_prob))
 
-    def compute_from_exp_samples(self, p, q, samples):
+        # This is a version that performs the computations in log space, this is desirable when probabilities
+        # are very small numbers that can lead to underflow
         # Obtain sample log probabilities
+        p_samples_logprob = p.log_prob(samples)
+        q_samples_logprob = q.log_prob(samples)
+        self.value = np.sum(self.compute_from_log_probs(p_samples_logprob, q_samples_logprob))
+
+        return self.value
+
+    @staticmethod
+    def compute_from_probs(p_samples_prob, q_samples_prob):
+        # Normalize sample probabilities
+        if np.sum(p_samples_prob) > 0:
+            p_samples_prob = p_samples_prob / np.sum(p_samples_prob)
+        if np.sum(q_samples_prob) > 0:
+            q_samples_prob = q_samples_prob / np.sum(q_samples_prob)
+        else:
+            return np.inf
+
+        # Compute discrete KL for each sample
+        res = p_samples_prob * np.log(p_samples_prob / q_samples_prob)
+        res[q_samples_prob <= 0] = 0
+        return res
+
+    @staticmethod
+    def compute_from_log_probs(p_samples_prob, q_samples_prob):
+        # Normalize sample probabilities in log space
+        p_samples_prob -= np.logaddexp.reduce(p_samples_prob)
+        q_samples_prob -= np.logaddexp.reduce(q_samples_prob)
+
+        # Compute discrete KL for each sample
+        res = np.exp(p_samples_prob) * (p_samples_prob - q_samples_prob)
+        res[np.isnan(q_samples_prob)] = np.inf
+        return res
+
+
+class CJSDivergence(CDivergence):
+    def __init__(self):
+        super().__init__()
+        self.name = "JSD"
+        self.type = "divergence"
+        self.is_symmetric = True
+        self.disjoint_support = False
+
+    def compute_from_samples(self, p, q, samples):
+        # Obtain sample probabilities
         p_samples_prob = p.prob(samples)
         q_samples_prob = q.prob(samples)
 
@@ -115,28 +161,9 @@ class CKLDivergence(CDivergence):
         else:
             return np.inf
 
-        # Normalize sample probabilities in log space
-        # p_samples_logprob_norm = p_samples_logprob - np.max(p_samples_logprob)
-        # q_samples_logprob_norm = q_samples_logprob - np.max(q_samples_logprob)
+        m_samples_prob = 0.5 * (p_samples_prob + q_samples_prob)
 
-        # Compute discrete KL for each sample
-        # res = np.exp(p_samples_logprob_norm) * (p_samples_logprob_norm - q_samples_logprob_norm)
-        res = p_samples_prob * np.log(p_samples_prob / q_samples_prob)
-        res[q_samples_prob <= 0] = 0
-        self.value = res.sum()
-        return self.value
+        res = 0.5 * CKLDivergence.compute_from_probs(p_samples_prob, m_samples_prob) + \
+              0.5 * CKLDivergence.compute_from_probs(q_samples_prob, m_samples_prob)
 
-    def compute_from_logsamples(self, p, q, samples):
-        # Obtain sample log probabilities
-        p_samples_prob = p.log_prob(samples)
-        q_samples_prob = q.log_prob(samples)
-
-        # Normalize sample probabilities in log space
-        p_samples_prob -= np.logaddexp.reduce(p_samples_prob)
-        q_samples_prob -= np.logaddexp.reduce(q_samples_prob)
-
-        # Compute discrete KL for each sample
-        res = np.exp(p_samples_prob) * (p_samples_prob - q_samples_prob)
-        res[q_samples_prob <= 0] = 0
-        self.value = res.sum()
-        return self.value
+        return res.sum()

tests/metrics/test_metric_divergence.py
@@ -27,10 +27,14 @@ class CMetricsTests(unittest.TestCase):
         self.metric.post()
         self.metric.reset()
 
-    def do_test(self, p, q, expected_res):
-        res = self.metric.compute(p=p, q=q, nsamples=1000000)
-        print("Test KL divergence. KLD:%.6f. Expected: %.6f" % (res, expected_res))
-        self.assertAlmostEqual(res, expected_res, places=3, msg="Divergence metric should be 0 for distributions that are the same")
+    def do_test(self, p, q, expected_res, tolerance=0.01):
+        res = self.metric.compute(p=p, q=q, nsamples=100000)
+        print("Test KL divergence. KLD:%.6f. Expected: %.6f. Error: %.3f%%" %
+              (res, expected_res, np.fabs(100*(res-expected_res)/expected_res)))
+
+        self.assertAlmostEqual(res, expected_res, delta=expected_res*tolerance,
+                               msg="Divergence metric should be within %.1f%% of the expected value %f" %
+                                   (tolerance*100, expected_res))
 
     @staticmethod
     def mvn_kld(mu0, mu1, sigma0, sigma1):