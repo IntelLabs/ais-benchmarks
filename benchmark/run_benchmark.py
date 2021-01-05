fab1015fb26908fd376bfad43e5b0e66a1cda3d9
Javier Felip Leon <javier.felip.leon@gmail.com>
2021-01-05 16:21:04 +0100

    DONE: TODO: make this paths relative to this file

--- benchmark/run_benchmark.py
+++ benchmark/run_benchmark.py
@@ -1,16 +1,21 @@
 import time
 import sys
+import pathlib
+from os import sep
 from benchmark.CBenchmark import CBenchmark
 from utils.misc import time_to_hms
 
 
 benchmark = CBenchmark()
 
-# TODO: make this paths relative to this file
-b_file = "def_benchmark.yaml"
-m_file = "def_methods.yaml"
-c_file = "def_config.yaml"
-out_file = "results.txt"
+# Make the default paths relative to this file
+def_path = str(pathlib.Path(__file__).parent.absolute()) + sep
+
+b_file = def_path + "def_benchmark.yaml"
+m_file = def_path + "def_methods.yaml"
+c_file = def_path + "def_config.yaml"
+
+out_file = def_path + "results.txt"
 
 if len(sys.argv) == 5:
     b_file = sys.argv[1]
