64696e49b6f870101d1dde4811eb3fdfbe05be15
Javier Felip Leon <javier.felip.leon@gmail.com>
2021-01-25 13:51:16 +0100

    Implement a job dispatcher script to run experiments in parallel with a configurable number of processes.
    Required the implementation of a helper class to perform non-blocking
    read from streams.

--- utils/misc.py
+++ utils/misc.py
@@ -1,6 +1,39 @@
 import numpy as np
+from threading import Thread
+from queue import Queue, Empty
+
 from sampling_methods.base import make_grid
 from distributions.mixture.CGaussianMixtureModel import CGaussianMixtureModel
+
+class CNonBlockingStreamReader:
+    def __init__(self, stream):
+        self._s = stream
+        self._q = Queue()
+
+        def _addqueue(s, q):
+            while True:
+                l = s.readline()
+                if l:
+                    q.put(l)
+
+        self._t = Thread(target=_addqueue, args=(self._s, self._q))
+        self._t.daemon = True
+        self._t.start()
+
+    def read_last_and_clear(self, keep_last=True):
+        val = None
+        while not self._q.empty():
+            val = self._q.get()
+
+        if val is not None and keep_last:
+            self._q.put(val)
+        return val
+
+    def read(self):
+        try:
+            return self._q.get(block=False)
+        except Empty:
+            return ""
 
 
 def generateRandomGMM(space_min, space_max, num_gaussians, sigma_min=(0.04, 0.04, 0.04), sigma_max=(0.05, 0.05, 0.05)):
